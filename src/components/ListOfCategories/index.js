bbc9f942c28713d7effe27559a141140dc768f0d
unknown <daniellopezj0327@gmail.com>
2021-01-04 10:07:27 -0500

    ...

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-daniellopez01.vercel.app/categories')
+      .then(res => res.json()).then(response => {
+        setCategories(response)
+      })
+  }, [])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [setShowFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => {
           return (
@@ -16,5 +35,12 @@ export const ListOfCategories = () => {
         })
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
