73eb726fde1a758fdee6c0172740ec2e9235dbb0
unknown <daniellopezj0327@gmail.com>
2021-01-04 16:22:48 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
+
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-
 import { Router } from '@reach/router'
 
 export const App = () => {
@@ -16,6 +17,7 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <NavBar />
     </>
   )
 }
